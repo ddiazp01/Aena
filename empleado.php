9ce087f79a076e8fc989c6bbe1fcfd5057a226b4
david <ddiazp01@gmail.com>
2018-05-02 09:51:24 +0200

    02/05/18 a las 9:51

---
+++ empleado.php
@@ -0,0 +1,34 @@
+<?php
+
+class Empleado {
+    var $nombre;
+    var $fechanac;
+
+    function __construct ($nombre,$fechanac){
+        $this->nombre = $nombre;
+        $this->fechanac = $fechanac;
+    }
+
+    function getNombre (){
+        return $this->nombre;
+    }
+
+    function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    function getFechaNac (){
+        return $this->fechanac;
+    }
+
+    function setFechaNac($fechanac){
+        $this->fechanac = $fechanac;
+    }
+
+    function getNumVuelos (){
+        return $this->numvuelos;
+    }
+
+}
+
+?>
